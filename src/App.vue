94b96579bdb73537ac26078c417bc53ee3c9b4fa
kinnitchi <igorivieira97@gmail.com>
2024-04-28 19:12:54 -0300

    ADD: Content and v-for list

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,19 @@
 <script lang="ts">
   import Banner from './components/Banner.vue';
+  import Content from './components/Content.vue';
 
   export default {
     name: 'App',
-    components: { Banner }
+    components: { Banner, Content }
   }
 </script>
 <template>
-  <Banner/>
+  <Banner />
+  <Content />
 </template>
-<style scoped></style>
+
+<style scoped>
+  .titulo {
+    color: red;
+  }
+</style>
